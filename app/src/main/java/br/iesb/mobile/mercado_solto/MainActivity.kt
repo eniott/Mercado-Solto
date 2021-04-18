9d740a8afa7d9d6fbbde9102b3719ebc369a8dfd
Enio Ribeiro Duraes Coelho <ercoelho@latam.stefanini.com>
2021-04-18 15:29:15 -0300

    adicionando tela home e autenticação firebase

--- app/src/main/java/br/iesb/mobile/mercado_solto/MainActivity.kt
+++ app/src/main/java/br/iesb/mobile/mercado_solto/MainActivity.kt
@@ -1,11 +1,43 @@
 package br.iesb.mobile.mercado_solto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btSignUpMain.setOnClickListener {
+            val chamasignup = Intent(this, SignUpActivity::class.java)
+            startActivity(chamasignup)
+        }
+
+        tvForgotMain.setOnClickListener {
+            val chamaforgot = Intent(this, ForgotActivity::class.java)
+            startActivity(chamaforgot)
+        }
+
+        btLoginMain.setOnClickListener {
+            val email = etEmailMain.text.toString()
+            val password = etPasswordMain.text.toString()
+
+            val auth = FirebaseAuth.getInstance()
+
+            val task_login = auth.signInWithEmailAndPassword(email, password)
+
+            task_login.addOnCompleteListener { resultado ->
+                if (resultado.isSuccessful){
+                    val ir_para_home = Intent(this, Home::class.java)
+                    startActivity(ir_para_home)
+                } else {
+                    Toast.makeText(this, "E-mail ou senha incorretos.", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 }
