ff53392428a0f548956f0322491b5eb099f5378f
Enio Ribeiro Duraes Coelho <ercoelho@latam.stefanini.com>
2021-06-16 17:02:47 -0300

    criando botões home

--- app/src/main/java/br/iesb/mobile/mercado_solto/view/activities/Home.kt
+++ app/src/main/java/br/iesb/mobile/mercado_solto/view/activities/Home.kt
@@ -1,12 +1,25 @@
 package br.iesb.mobile.mercado_solto.view.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.iesb.mobile.mercado_solto.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        btCategoriasHome.setOnClickListener {
+            val chamacategorias = Intent(this, CategoriasActivity::class.java)
+            startActivity(chamacategorias)
+        }
+
+        btCompletarPerfilHome.setOnClickListener {
+            val chamaperfil = Intent(this, PerfilActivity::class.java)
+            startActivity(chamaperfil)
+        }
+
     }
 }
