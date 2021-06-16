aaca64d8cf7c5b451dbe44cf9d5ec0540c7e4f6a
Enio Ribeiro Duraes Coelho <ercoelho@latam.stefanini.com>
2021-06-16 00:19:44 -0300

    adicionando novas activities

---
+++ app/src/main/java/br/iesb/mobile/mercado_solto/view/activities/PerfilActivity.kt
@@ -0,0 +1,12 @@
+package br.iesb.mobile.mercado_solto.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.iesb.mobile.mercado_solto.R
+
+class PerfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil)
+    }
+}
