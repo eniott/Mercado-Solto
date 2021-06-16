50e25c655b53f95cd6873ecbeea66c0ab625cbe3
Enio Ribeiro Duraes Coelho <ercoelho@latam.stefanini.com>
2021-06-16 19:03:07 -0300

    adicionando menu de categorias

--- app/src/main/java/br/iesb/mobile/mercado_solto/view/activities/Home.kt
+++ app/src/main/java/br/iesb/mobile/mercado_solto/view/activities/Home.kt
@@ -3,6 +3,8 @@ package br.iesb.mobile.mercado_solto.view.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import br.iesb.mobile.mercado_solto.R
 import kotlinx.android.synthetic.main.activity_home.*
 
@@ -20,6 +22,23 @@ class Home : AppCompatActivity() {
             val chamaperfil = Intent(this, PerfilActivity::class.java)
             startActivity(chamaperfil)
         }
+
+        val spiner: Spinner = findViewById(R.id.categorias)
+
+        //val categorias = ListOf<String>("teste, teste2, teste3")
+
+        val categorias = ArrayList<String>()
+        categorias.add("Mercados")
+        categorias.add("Restaurantes")
+        categorias.add("Roupas")
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categorias)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spiner.adapter = arrayAdapter
+
+
+
 
     }
 }
